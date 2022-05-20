d0bd0295bd141c81aa80cf2b9256fe39b54ba84f
Manuel Baez Diaz <baezdiazm@yahoo.com>
2022-05-19 23:08:21 -0400

    commit tree 293c62410184d7aed38ea5092502bd40a738a71f

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
+import items from "../data/items";
 
 function Item({ name, category }) {
+
+  const [itemState, setItemState] = useState("")
+
+  function handleAddToCart() {
+    if(itemState === "") {
+      setItemState((itemState) => "in-cart")
+    }
+  }
   return (
-    <li className="">
+    <li className={itemState}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleAddToCart}>Add to Cart</button>
     </li>
   );
 }
